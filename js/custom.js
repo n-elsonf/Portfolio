de5bd929664700d5b6edb2f3d56f5f63aff2f1bc
Nelson <nf243@cornell.edu>
2024-05-14 02:34:54 -0400

    made navbar

--- js/custom.js
+++ js/custom.js
@@ -73,11 +73,11 @@ particlesJS("bg", {
     "detect_on": "canvas",
     "events": {
       "onhover": {
-        "enable": true,
+        "enable": false,
         "mode": "grab"
       },
       "onclick": {
-        "enable": true,
+        "enable": false,
         "mode": "push"
       },
       "resize": true
